a8783bee6c3a046a90c807dabb29c5da31994b3c
Yaniv Pinchas <yanivps@gmail.com>
2018-08-21 12:53:01 +0300

    Added how it works to home page

--- client/src/app/core/components/beta/beta.component.ts
+++ client/src/app/core/components/beta/beta.component.ts
@@ -1,26 +1,42 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from '../../../shared/services/data.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { BetaLoginComponent } from '../beta-login/beta-login.component';
 
 @Component({
   selector: 'app-beta',
-  templateUrl: './beta.component.html',
-  styleUrls: ['./beta.component.css']
+  templateUrl: '../home/home.component.html',
+  styleUrls: ['../home/home.component.css']
 })
 export class BetaComponent implements OnInit, OnDestroy {
   betaLoggedIn: boolean = false;
+  fragment: string;
 
   constructor(
     private dataService: DataService,
     private router: Router,
+    private route: ActivatedRoute,
     private modalService: NgbModal) {
     this.betaLoggedIn = localStorage.getItem("betaLogin") == "4646";
   }
 
   ngOnInit() {
     document.body.classList.add('home-background');
+    this.route.fragment.subscribe(fragment => {
+      this.fragment = fragment;
+      if (!this.fragment) {
+        window.scrollTo(0, 0);
+      }
+    });
+  }
+
+  ngAfterViewChecked(): void {
+    try {
+      if (this.fragment) {
+        document.querySelector('#' + this.fragment).scrollIntoView();
+      }
+    } catch (e) { }
   }
 
   ngOnDestroy() {
--- client/src/app/core/components/home/home.component.ts
+++ client/src/app/core/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from '../../../shared/services/data.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { BetaLoginComponent } from '../beta-login/beta-login.component';
 
@@ -10,17 +10,33 @@ import { BetaLoginComponent } from '../beta-login/beta-login.component';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  betaLoggedIn: boolean = false;
+  fragment: string;
 
   constructor(
     private dataService: DataService,
-    private router: Router) {
+    private router: Router,
+    private route: ActivatedRoute) {
       localStorage.setItem("betaLogin", "4646");
     }
 
   ngOnInit() {
     document.body.classList.add('home-background');
+    this.route.fragment.subscribe(fragment => {
+      this.fragment = fragment;
+      if (!this.fragment) {
+        window.scrollTo(0, 0);
+      }
+    });
   }
+
+  ngAfterViewChecked(): void {
+    try {
+      if (this.fragment) {
+        document.querySelector('#' + this.fragment).scrollIntoView();
+      }
+    } catch (e) { }
+  }
+
 
   ngOnDestroy() {
     document.body.classList.remove('home-background');
